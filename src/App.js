97bb79d52c5703043dcd563aad1366860d37cb05
joao <jgabrielsousa@gmail.com>
2018-03-28 00:29:22 -0300

    novo componente contador

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contador from './Componentes/Contador/Contador'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Bem vindo ao React</h1>
         </header>
+        <div><Contador /></div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Componentes/Contador/Contador.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Contador extends React.Component
+{
+    render(){
+        return (<h1>Meu contador 0</h1>)
+    }
+}
+
+export default Contador
